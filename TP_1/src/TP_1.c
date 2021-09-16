d3f21e963fbbab986a3bf326665cd7eb04db1656
Gabriel <gaby_servia@hotmail.com>
2021-09-16 01:49:04 -0300

    Primer commit de la carpeta TP_1. Solo es una prueb para testear cuenta de Github. No hay avavances

---
+++ TP_1/src/TP_1.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : TP_1.c
+ Author      : Gabriel Servia
+ University	 : UTN FRA
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	setbuf(stdout, NULL);
+
+	printf("Menu calculadora\n 1. Suma\n 2. Resta\n 3. Multiplicacion\n 4. Division\n 5. Factorial");
+
+	return EXIT_SUCCESS;
+}
