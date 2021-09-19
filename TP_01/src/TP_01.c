acbeb248d2fd79a6098128c32e6fd3938933910f
Gabriel <gaby_servia@hotmail.com>
2021-09-19 20:42:12 -0300

    Probando la subida de archivos al repositorio remoto

---
+++ TP_01/src/TP_01.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : TP_01.c
+ Author      : Gabriel Servia
+ University  : UTN FRA
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	puts("Trabajo practico 1 - Calculadora");
+
+	return EXIT_SUCCESS;
+}
